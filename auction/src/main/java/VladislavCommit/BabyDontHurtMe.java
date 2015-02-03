c8fa3db9266888caf0c918596b08a2a3841fdd8f
MarksNamavirs <myProject9>
2015-02-03 20:07:24 +0200

    deleted trash

--- auction/src/main/java/VladislavCommit/BabyDontHurtMe.java
+++
@@ -1,7 +0,0 @@
-package VladislavCommit;
-
-/**
- * Created by Vladislav on 2/3/2015.
- */
-public class BabyDontHurtMe {
-}
--- auction/src/main/java/lv/javaguru/java2/Sasa/Sasa.java
+++
@@ -1,7 +0,0 @@
-package lv.javaguru.java2.Sasa;
-
-/**
- * Created by Koloshko on 2/3/2015.
- */
-public class Sasa {
-}

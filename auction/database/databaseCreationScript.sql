4e64e8c476cd050d88159bc910f409c773588d7e
Vladislav <Streamline28@mail.ru>
2015-02-16 14:36:21 +0200

    Product database V1 added!

--- auction/database/databaseCreationScript.sql
+++ auction/database/databaseCreationScript.sql
@@ -18,6 +18,24 @@ CREATE TABLE IF NOT EXISTS `Java2_test`.`users` (
 )
 ENGINE = InnoDB
 AUTO_INCREMENT = 1002;
+-- ------------------------------------------------------
+-- Product table
+-- ------------------------------------------------------
+drop table if exists `Java2_test`.`products`;
+
+create table if not exists  `Java2_test`.`products`(
+  `ProductID` int(11) not null auto_increment,
+  `Name` varchar(35) not null,
+  `Description` varchar(3000),
+  `OwnerID` int(11) not null,
+  primary key(`ProductID`),
+  foreign key(`OwnerID`) references users(`UserID`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1002;
+
+-- -----------------------------------------------------
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;

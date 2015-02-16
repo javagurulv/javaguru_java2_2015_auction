f88de0fc15f514523bda47fee3b078604e3caa0c
Vladislav <Streamline28@mail.ru>
2015-02-16 14:18:18 +0200

    BaseDAO genetic interface added!

--- auction/src/main/java/lv/javaguru/java2/database/jdbc/ProductDAOImpl.java
+++ auction/src/main/java/lv/javaguru/java2/database/jdbc/ProductDAOImpl.java
@@ -3,6 +3,7 @@ package lv.javaguru.java2.database.jdbc;
 import lv.javaguru.java2.database.DBException;
 import lv.javaguru.java2.database.ProductDAO;
 import lv.javaguru.java2.database.UserDAO;
+import lv.javaguru.java2.domain.Product;
 import lv.javaguru.java2.domain.User;
 
 import java.util.List;
@@ -11,5 +12,29 @@ import java.util.List;
  * Created by Vladislav on 2/15/2015.
  */
 public class ProductDAOImpl extends DAOImpl implements ProductDAO {
-    
+
+    @Override
+    public void create(Product product) throws DBException {
+        
+    }
+
+    @Override
+    public Product getById(Long id) throws DBException {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) throws DBException {
+
+    }
+
+    @Override
+    public void update(Product product) throws DBException {
+
+    }
+
+    @Override
+    public List<Product> getAll() throws DBException {
+        return null;
+    }
 }

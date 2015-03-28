8451f2961a337397cb79a98fd92eb9bda5591021
Vladislav <Streamline28@mail.ru>
2015-03-29 01:13:30 +0300

    Security works nice!

--- auction/src/main/java/lv/javaguru/java2/servlet/SecurityConfig.java
+++ auction/src/main/java/lv/javaguru/java2/servlet/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/prot/index")
+                .defaultSuccessUrl("/index")
                 .permitAll()
                 .and()
             .logout()
